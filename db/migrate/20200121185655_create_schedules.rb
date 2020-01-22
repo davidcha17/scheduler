150ee6ca89b6deaecd8874caedd032d112bf65ed
David Cha <davidcha17@yahoo.com>
2020-01-22 14:09:51 -0500

    omg

--- db/migrate/20200121185655_create_schedules.rb
+++ db/migrate/20200121185655_create_schedules.rb
@@ -2,7 +2,7 @@ class CreateSchedules < ActiveRecord::Migration[6.0]
   def change
     create_table :schedules do |t|
       t.integer :user_id
-
+ 
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,13 @@ ActiveRecord::Schema.define(version: 2020_01_21_195324) do
   end
 
   create_table "days", force: :cascade do |t|
-    t.string "name"
+    t.string "monday"
+    t.string "tuesday"
+    t.string "wednesday"
+    t.string "thursday"
+    t.string "friday"
+    t.string "saturday"
+    t.string "sunday"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -52,7 +58,30 @@ ActiveRecord::Schema.define(version: 2020_01_21_195324) do
   end
 
   create_table "time_slots", force: :cascade do |t|
-    t.string "slot"
+    t.integer "one"
+    t.integer "two"
+    t.integer "three"
+    t.integer "four"
+    t.integer "five"
+    t.integer "six"
+    t.integer "seven"
+    t.integer "eight"
+    t.integer "nine"
+    t.integer "ten"
+    t.integer "eleven"
+    t.integer "twelve"
+    t.integer "thirteen"
+    t.integer "fourteen"
+    t.integer "fifteen"
+    t.integer "sixteen"
+    t.integer "seventeen"
+    t.integer "eighteen"
+    t.integer "nineteen"
+    t.integer "twenty"
+    t.integer "twenty_one"
+    t.integer "twenty_two"
+    t.integer "twenty_three"
+    t.integer "twenty_four"
     t.integer "schedule_time_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -63,7 +92,6 @@ ActiveRecord::Schema.define(version: 2020_01_21_195324) do
     t.integer "age"
     t.string "pw"
     t.string "img_url"
-    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
