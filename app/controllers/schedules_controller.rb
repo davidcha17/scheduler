1e1ff73dec453dc6c77b1eb8bdb438437f188d2e
David Cha <davidcha17@yahoo.com>
2020-01-22 09:27:06 -0500

    status

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,2 +1,37 @@
 class SchedulesController < ApplicationController
+    def index
+        @schdedules = Schedule.all
+    end 
+
+    def show 
+        @schdedule = Schedule.find(params[:id])
+    end
+
+    def new
+        @schdedule = Schedule.new()
+    end
+
+    def create
+        @schdedule = Schedule.create()
+    end
+
+    def edit
+        @schdedule = Schedule.find(params[:id])
+    end
+
+    def update
+        schdedule = Schedule.find(params[:id])
+        schdedule.update
+    end
+
+    def destroy
+        schdedule = Schedule.find(params[:id])
+        schdedule.destroy
+    end
+
+    private
+
+    def find_schedule
+        @schdedule = Schedule.find(params[:id])
+    end
 end
